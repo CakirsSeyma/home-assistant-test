4ed6d061d230054cebdcf3cf582bbc8ea7c14413
Şeyma Çakır <cakirs2001@gmail.com>
2026-05-04 11:17:27 +0300

    TEST

--- custom_components/desi/const.py
+++ custom_components/desi/const.py
@@ -9,18 +9,20 @@ DOMAIN = "desi"
 LOGIN_METHODS = ["phone", "email"]
 DEFAULT_LOGIN_METHOD = "email"
 
-WEB_URL = "https://__DESI_WEB_DOMAIN__"
-API_URL = "https://__DESI_API_DOMAIN__"
 
-AUTH_URI = f"{WEB_URL}/ds/sign-in-for-home-assistant"
-TOKEN_URI = f"{API_URL}/api/third_part_devices/ds/home_assistant/token"
-FULLFILMENT_API_URI = f"{API_URL}/api/third_part_devices/ds/home_assistant/control"
+AUTH_URI = "https://web.desisteely.com/ds/sign-in-for-home-assistant"
+TOKEN_URI = "https://desisteely.com/api/third_part_devices/ds/home_assistant/token"
+FULLFILMENT_API_URI = (
+    "https://desisteely.com/api/third_part_devices/ds/home_assistant/control"
+)
+API_URL = "https://desisteely.com"
 SOCKET_PATH = "/api/third_part_devices/ds/home_assistant/ws"
-WS_URL = f"{API_URL}{SOCKET_PATH}"
+WS_URL = API_URL + SOCKET_PATH
+
 
 PLATFORMS = [
     Platform.LOCK,
     Platform.ALARM_CONTROL_PANEL,
     Platform.SWITCH,
-    Platform.SENSOR,
+    Platform.SENSOR
 ]
